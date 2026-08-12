99c0a2d9584a6b12c3988002677fc9b72e3c906c
Muhaqqiq <118525067+Muhaqqiq@users.noreply.github.com>
2026-08-12 01:13:41 +0100

    Add service worker for asset caching
    Implement service worker for caching assets.

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'expense-tracker-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
